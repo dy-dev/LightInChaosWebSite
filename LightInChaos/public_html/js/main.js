dd8d24e24e6e237d6f50cbd447e524b516b625e9
dy-dev <dominique.yolin@free.fr>
2016-11-22 16:11:59 +0100

    Light In Chaos WebSite first commit

---
+++ LightInChaos/public_html/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $('#my-video').backgroundVideo();
+    $('.service-list').toggle('height');
+});
+
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+
+    var offset = 180;
+    console.log(target.offset().top);
+    console.log($('#video-wrap').offset().top);
+
+    if (target.attr('id') === $('#video-wrap').attr('id'))
+    {
+        offset = -10;
+    }
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - offset
+        }, 1000);
+    }
+});
+
+function checkTopPos()
+{
+    var height = $(window).scrollTop();
+
+    if (height > 0)
+    {
+        $('nav').removeClass('active');
+        $('.wrapper').removeClass('active');
+        $('#logo_container').removeClass('active');
+    }
+    else
+    {
+        $('nav').addClass('active');
+        $('.wrapper').addClass('active');
+        $('#logo_container').addClass('active');
+    }
+}
+
+function toggle(myDiv)
+{
+   $(myDiv).animate({height:'toggle'});
+}
