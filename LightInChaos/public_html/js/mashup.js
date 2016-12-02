ff456ffc1c78769c6c811662371cfb77d0757be2
dy-dev <dominique.yolin@free.fr>
2016-12-02 11:59:18 +0100

    Mashup randomly as backgound

---
+++ LightInChaos/public_html/js/mashup.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(document).ready(function () {
+    var randVal = getRandomInt(1, 3);
+    var fileName = "videos/Mashup" + randVal;
+
+    $('#mp4').attr("src", fileName + ".mp4");
+    $('#wbm').attr("src", fileName + ".webm");
+});
