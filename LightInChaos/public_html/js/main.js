25653c791f182caf82ff832950013fff5ff6f069
dy-dev <dominique.yolin@free.fr>
2016-12-30 01:41:59 +0100

    responsive tweeking

--- LightInChaos/public_html/js/main.js
+++ LightInChaos/public_html/js/main.js
@@ -3,10 +3,21 @@ $(document).ready(function () {
     $('#my-video').backgroundVideo();
     $('.service-list').toggle('height');
     $('#info-content').toggle('left');
+    $('#horiz-line').toggle('width');
+    $('#vert-line').toggle('height');
+    $('#left-line').toggle('width');
+    $('#right-line').toggle('width');
+    $('#manifest').toggle('height');
+
     $('#info-bar').toggle('visibility');
     // $('#text-bar').toggle('width');
     //console.log("add scroll event");
     $(window).scroll(checkTopPos);
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 160
+    });
 });
 
 $('a[href^="#"]').on('click', function (event) {
@@ -46,20 +57,64 @@ function checkTopPos()
      }*/
 }
 $('#sidebar-button').click(function () {
-    $('#info-content').animate({left: 'toggle'});
+
     if ($("#info-content").position().left === 0)
     {
         $('#sidebar-button').removeClass('custom-open-button');
         $('#sidebar-button').addClass('custom-close-button');
-
+        $('#info-content').animate({left: 'toggle'}, "slow", growHorizLine);
     }
     else
     {
         $('#sidebar-button').removeClass('custom-close-button');
         $('#sidebar-button').addClass('custom-open-button');
-
+        shrinkContent();
     }
 });
+
+function growHorizLine() {
+    $('#horiz-line').animate({width: 'toggle'}, growVertLine);
+}
+
+
+function growVertLine() {
+    $('#vert-line').animate({height: 'toggle'}, growStartContent);
+}
+
+function growStartContent() {
+    $('#left-line').animate({width: 'toggle'});
+    $('#right-line').animate({width: 'toggle'}, growContent);
+}
+
+function growContent()
+{
+    $('#manifest').animate({height: 'toggle'});
+}
+
+
+function shrinkContent()
+{
+    $('#manifest').animate({height: 'toggle'}, shrinkStartContent);
+}
+
+function shrinkStartContent() {
+    $('#left-line').animate({width: 'toggle'});
+    $('#right-line').animate({width: 'toggle'}, shrinkVertLine);
+}
+
+function shrinkVertLine() {
+    $('#vert-line').animate({height: 'toggle'}, shrinkHorizLine);
+}
+
+function shrinkHorizLine() {
+    $('#horiz-line').animate({width: 'toggle'}, shrinkInfoContent);
+}
+
+function shrinkInfoContent() {
+    $('#info-content').animate({left: 'toggle'});
+}
+
+
 //$('#sidebar-button').click(toggle('#info-content'));
 //function toggle(myDiv)
 //{
@@ -81,3 +136,13 @@ function manageInfoDisplay()
      }
      toggle('#info-bar');*/
 }
+
+$('input[type="checkbox"]').on('change', function() {
+    if($(this).is(":checked")) {
+       $("#backdiv").removeClass('displayon');
+    }
+    else
+    {
+       $("#backdiv").addClass('displayon');
+    }
+});
